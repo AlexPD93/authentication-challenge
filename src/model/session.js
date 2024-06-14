a1ca8fd8c00cdb5b16436fab7c997e5fba4e7f4a
Alex <alexperezdavies@gmail.com>
2024-06-14 10:59:18 +0100

    complete challenge 1

--- src/model/session.js
+++ src/model/session.js
@@ -1,10 +1,21 @@
 const db = require("../database/db.js");
+const crypto = require("node:crypto");
 
-const insert_session = db.prepare(`SELECT 1`);
+const insert_session = db.prepare(/*sql*/ `
+INSERT INTO sessions (id, user_id, expires_at)
+VALUES (
+  $id,
+  $user_id,
+  DATE('now', '+7 day'))
+`);
 
 function createSession(user_id) {
-  // to-do
+  const id = crypto.randomBytes(18).toString("base64");
+  insert_session.run({ id, user_id });
+  return id;
 }
+
+// console.log(createSession(1));
 
 const select_session = db.prepare(`
   SELECT id, user_id, expires_at

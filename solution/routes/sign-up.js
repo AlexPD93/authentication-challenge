eacf4463bdffb58c07f21ac56def15269d42da81
Alex <alexperezdavies@gmail.com>
2024-06-14 12:51:24 +0100

    complete challenge 4

--- solution/routes/sign-up.js
+++ solution/routes/sign-up.js
@@ -35,7 +35,7 @@ function post(req, res) {
       const session_id = createSession(user.id);
       res.cookie("sid", session_id, {
         signed: true,
-        maxAge: 1000 * 60,
+        maxAge: 1000 * 60 * 60 * 24 * 7,
         sameSite: "lax",
         httpOnly: true,
       });
